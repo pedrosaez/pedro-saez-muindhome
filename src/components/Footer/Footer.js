a85f943406d667c4a1c4591684051264d003bc01
pedro saez <pedrosaez02@gmail.com>
2022-10-20 23:16:04 -0300

    agregando css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>Hola Footer</div>
+    )
+}
+
+export default Footer
---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import './Main.css';
+
+const Main = () => {
+
+    return (
+        <main className="main">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi architecto quae quibusdam quasi totam quas doloremque dolore pariatur magni quis perferendis provident iure at adipisci dolorem dignissimos, quam nemo officia?</p>
+        </main>
+    )
+}
+
+export default Main
